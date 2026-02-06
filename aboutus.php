4f503ac24f72b5c7f0851c915e82d13d8be1a1c7
koiralasushant11-sketch <koiralasushant11@gmail.com>
2026-02-06 15:24:33 +0200

    Update aboutus.php

---
+++ aboutus.php
@@ -0,0 +1,9 @@
+<?php
+include 'header.php';
+?>
+    <main>
+        <h2>About US</h2>
+        <p> It includes a header and footer, with a body area where you can add information for your users.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia, recusandae aliquid mollitia, nulla sequi iusto reprehenderit rem, autem minima et expedita debitis? Porro, mollitia. Laborum atque eaque hic voluptate explicabo.</p>
+    </main>
+    <?php include 'footer.php'; ?>
